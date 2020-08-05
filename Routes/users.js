bc7016809ba659e41dc32e7d26691b07a6bd7107
OukaMiyuki <asukaloid.blog@gmail.com>
2020-08-06 01:00:26 +0700

    Check existing user

--- Routes/users.js
+++ Routes/users.js
@@ -1,5 +1,6 @@
 const { User, validate } = require('../Models/User');
 const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
@@ -13,7 +14,7 @@ router.get('/register', (req, res) => {
     res.render("register");
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async(req, res) => {
     const { name, email, password, password2 } = req.body;
     const { error } = validate(req.body);
 
@@ -27,7 +28,25 @@ router.post('/register', (req, res) => {
             password2
         });
     } else {
-        return res.send("Berhasil!");
+        const checkUserExists = await User.findAndCountAll({
+            where: {
+                [Op.or]: [
+                    { name: req.body.name },
+                    { email: req.body.email }
+                ]
+            }
+        }).catch((err) => console.log(err.message));
+        if (checkUserExists.count > 0) {
+            const errors = 'Username or Email has already been registered!';
+            return res.status(400).render('register', {
+                errors,
+                name,
+                email,
+                password,
+                password2
+            });
+        }
+
     }
 });
 
