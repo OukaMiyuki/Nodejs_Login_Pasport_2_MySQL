1a7e37a281c21a2b3769aac7cb4f4158824ae353
OukaMiyuki <asukaloid.blog@gmail.com>
2020-08-06 12:00:08 +0700

    Show logged in user

--- Routes/index.js
+++ Routes/index.js
@@ -7,7 +7,9 @@ router.get('/', forwardAuthenticated, (req, res) => {
 });
 //dashboard page
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
-    res.render("dashboard");
+    res.render("dashboard", {
+        name: req.user.name
+    });
 });
 
 module.exports = router;
